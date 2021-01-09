c22623b17415ef7f07880311aef3ea535a603618
vhjc <73879091+vhjc@users.noreply.github.com>
2021-01-09 16:14:43 +0800

    Update new.js

--- new.js
+++ new.js
@@ -10,16 +10,10 @@ function searchbing()
 	window.open("https://bing.com/search?q="+bing.value);
 	location.reload();
 }
-function searchgoogle()
-{
-	var google=document.getElementById("google");
-	window.open("https://www.google.com/search?q="+google.value);
-	location.reload();
-}
 function searchgooglem()
 {
 	var googlem=document.getElementById("googlem");
-	window.open("https://g.dgclouds.net/search?q="+googlem.value);
+	window.open("https://www.google.com/search?q="+googlem.value);
 	location.reload();
 }
 function zhihu()
